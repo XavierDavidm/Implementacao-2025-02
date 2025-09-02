82179fbc9838bcff89ca24f4f1d838faa1eb2294
XavierDavidm <davidmoreirax15@hotmail.com>
2025-09-01 21:20:46 -0300

    html

--- web/app/hello/routes.py
+++ web/app/hello/routes.py
@@ -1,11 +1,10 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template
 hello_bp = Blueprint('hello',__name__)
 
 
 @hello_bp.route('/')
-def index1():
-    return "Olá, Mundo!"
+def index():
+    return render_template('index.html') #retorna o html da file index.html
 @hello_bp.route('/sobre')
 def index2():
     return "Olá, David"
