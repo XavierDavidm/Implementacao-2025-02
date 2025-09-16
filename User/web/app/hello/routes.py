4b397accd117c38e50354f7bd2d2ddf2a44094e5
XavierDavidm <davidmoreirax15@hotmail.com>
2025-09-15 21:43:12 -0300

    aula 7

--- User/web/app/hello/routes.py
+++ User/web/app/hello/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, Response, redirect, url_for
-from ..models import User, Tarefa, db
+from ..models import User, db
 #from web.app.models import User, db
 
 
@@ -9,8 +9,7 @@ hello_bp = Blueprint('hello',__name__)
 @hello_bp.route('/')
 def index():
     usuarios=User.query.all()
-    tarefas=Tarefa.query.all()
-    return render_template('index.html', usuarios=usuarios,tarefas=tarefas) #retorna o html da file index.html
+    return render_template('index.html', usuarios=usuarios) #retorna o html da file index.html
 
 #faz a request do form do index.html
 @hello_bp.route('/novoUsuario', methods=['POST'])
@@ -41,29 +40,5 @@ def editarUsuario(usuario_id):
         db.session.commit()
     return redirect(url_for('hello.index'))
 
-#tarefa criação e leitura
-@hello_bp.route('/novaTarefa', methods=['POST'])
-def novaTarefa():
-    descricao_tarefa = request.form['desc_tarefa']
-    nova_tarefa = Tarefa(descricao = descricao_tarefa)
-    db.session.add(nova_tarefa)
-    db.session.commit()
-    return redirect('/')
-
-@hello_bp.route('/removerTarefa/<int:tarefa_id>', methods=['POST'])
-def removerTarefa(tarefa_id):
-    tarefa=Tarefa.query.get(tarefa_id)
-    if tarefa:
-        db.session.delete(tarefa)
-        db.session.commit()
-    return redirect(url_for('hello.index'))
-
-@hello_bp.route('/editarTarefa/<int:tarefa_id>', methods=['POST'])
-def editarTarefa(tarefa_id):
-    tarefa=Tarefa.query.get(tarefa_id)
-    if tarefa:
-        tarefa.descricao = request.form['desc_tarefa']
-        db.session.commit()
-    return redirect(url_for('hello.index'))
 
 
--- User/web/app/models.py
+++ User/web/app/models.py
@@ -6,7 +6,3 @@ class User(db.Model): #modelo da tabela
     #campos(tipo,chaves etc)
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-
-class Tarefa(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    descricao = db.Column(db.String(80), unique=True, nullable=False)
